2338438cda6d712f0f28d028efdfb768af19c4fa
Andrew Hudson <andrewhudson@Andrews-MacBook-Air-5.local>
2023-04-02 07:37:41 -0500

    haha

--- scripts/index.js
+++ scripts/index.js
@@ -19,10 +19,10 @@ async function expand(id){
             let html = document.getElementById(id).innerHTML
             Employees.forEach(function(person) {
                 html+= `<div data-role="collapsible">
-                <div data-role="collapsible" class="ui-collapsible ui-collapsible-inset ui-corner-all ui-collapsible-themed-content"><h1 class="ui-collapsible-heading" onclick="expand('${person.id}')"><a href="#" class="ui-collapsible-heading-toggle ui-btn ui-btn-icon-left ui-btn-inherit ui-icon-minus">${person.fname} ${person.lname}: ${person.position}<span class="ui-collapsible-heading-status"> click to collapse contents</span></a></h1><div class="ui-collapsible-content ui-body-inherit" aria-hidden="false">
-                <div data-role="collapsible" id='${person.id}'>
+                <div data-role="collapsible" class="ui-content ui-collapsible ui-collapsible-inset ui-corner-all ui-collapsible-themed-content"><h1 class="ui-collapsible-heading" onclick="expand('${person.id}')"><a href="#" class="ui-collapsible-heading-toggle ui-btn ui-btn-icon-left ui-btn-inherit ui-icon-minus">${person.fname} ${person.lname}: ${person.position}<span class="ui-collapsible-heading-status"> click to collapse contents</span></a></h1><div class="ui-collapsible-content ui-body-inherit" aria-hidden="false">
+                <div data-role="collapsible" id='${person.id}'  >
                 <button data-bs-toggle="modal" data-bs-target="#infoModel" onclick="updateModel('${person.fname}', '${person.lname}', '${person.email}', '${person.position}', '${person.locationcode}', '${person.location}')">My Info</button>
-                <p>I Manage these people</p>
+                
                 </div>
                 </div>
                 </div>
